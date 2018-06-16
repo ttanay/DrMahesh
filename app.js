380044771edab55642fec41c9ca48fdbec8cac63
ttanay <ttanay100@gmail.com>
2018-06-16 18:46:04 +0530

    mysql query done

--- app.js
+++ app.js
@@ -1,44 +1,28 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const mysql = require('mysql');
 const nodemailer = require('nodemailer');
 const credentials = require('./credentials.js');
 
 app = express();
 
-//DB setup
-mongoose.connect('mongodb://localhost:27017/lattice');
+//helpers
+function cleanData(queryResult) {
 
-//Schemas
-var patientSchema = new mongoose.Schema({
-  prescriptionDate: Date,
-  response: {
-    createdOn: Date,
-    responseText: String,
-  },
+};
+
+//DB
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password123',
+  database: 'lattice',
 });
 
-var Patient = mongoose.model('Patient', patientSchema);
+connection.connect();
 
-
-//GENRATOR FN
-function populateDB(){
-  let patient = Patient({
-    prescriptionDate: new Date(2018, 01, Math.floor(Math.random()*10)),
-    response: {
-      createdOn: new Date(2018, 01 + Math.floor(Math.random()*3), 10 + Math.floor(Math.random()*10)),
-      responseText: 'response',
-    }
-  });
-
-  patient.save(function (err){
-    if (err) console.log(err)
-  });
-}
-
-
-for(let i = 0; i < 15; i++)
-  populateDB();
-
+connection.query('SELECT responses.response_id,responses.patient_id,created_on,prescription_date from responses inner join patients on responses.patient_id = patients.patient_id inner join response_details on responses.response_id = response_details.response_id order by patients.patient_id;', (error, results, fields) => {
+  console.log(results);
+});
 //EMAIL
 var transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -50,7 +34,7 @@ var transporter = nodemailer.createTransport({
 
 var mailOptions = {
   from: credentials.username,
-  to: '<DR MAHESH EMAIL>',
+  to: 'tanayrao007@gmail.com',
   subject: 'Unfilled surveys',
 }
 
@@ -59,25 +43,19 @@ app.get('/sendmail/', (req, res) => {
 
   let unfilled = [];
 
-  Patient.find({}).exec((err, patients) => {
-    patients.forEach(function(patient) {
-      daysSincePrescription = patient.response.createdOn - patient.prescriptionDate;
-      daysSincePrescription = daysSincePrescription / (1000 * 3600 * 24);
-      if(daysSincePrescription > 35){
-        unfilled.push(patient);
-      }
-    });
-    mailOptions.html = JSON.stringify({data: unfilled});
-		res.setHeader('Content-Type', 'application/json');
-  	res.send(JSON.stringify({data: unfilled}))
-  	transporter.sendMail(mailOptions, (err, info) => {
-    	if(err)
-      	console.log(err);
-    	else
-      	console.log(info);
-  	});
-  });
+  // unfilled = QUERY_RESULT
 
+  //Construct JSON data
+
+  mailOptions.html = JSON.stringify({data: unfilled});
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({data: unfilled}));
+  transporter.sendMail(mailOptions, (err, info) => {
+    if(err)
+      console.log(err);
+    else
+      console.log(info);
+  });
 });
 
 //Listen
