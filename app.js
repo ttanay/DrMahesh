09c0100174974323e5854a5c53abd3ea92c4bfc9
ttanay <ttanay100@gmail.com>
2018-06-16 22:41:04 +0530

    mysql result processed

--- app.js
+++ app.js
@@ -6,8 +6,37 @@ const credentials = require('./credentials.js');
 app = express();
 
 //helpers
-function cleanData(queryResult) {
+var cleanResult = function(current, result){
+  current = {
+    patient_id: result.patient_id,
+    prescription_date: result.prescription_date,
+    responses: [
+      {
+        response_id: result.response_id,
+        created_on: result.created_on,
+        text: result.response_text,
+      }
+    ]
+  };
+  return current;
+};
 
+var filterData = function(dataset){
+  let filteredData = [];
+  dataset.forEach((data) => {
+    let recentResponses = data.responses.slice(Math.max(data.responses.length -2, 1))
+    if(recentResponses >= 1){
+      let daysSinceSurvey = Math.abs(Date.parse(recentResponses[0]) - (new Date()));
+      if(daysSinceSurvey > 7*5)
+        filteredData.push(data);
+    }
+    else{
+      let daysSincePrescription = Math.abs(Date.parse(data.prescription_date) - (new Date()));
+      if(daysSincePrescription > 7*5)
+        filteredData.push(data);
+    }
+  });
+  return filteredData;
 };
 
 //DB
@@ -20,9 +49,7 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('SELECT responses.response_id,responses.patient_id,created_on,prescription_date from responses inner join patients on responses.patient_id = patients.patient_id inner join response_details on responses.response_id = response_details.response_id order by patients.patient_id;', (error, results, fields) => {
-  console.log(results);
-});
+
 //EMAIL
 var transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -41,21 +68,46 @@ var mailOptions = {
 //Routes
 app.get('/sendmail/', (req, res) => {
 
-  let unfilled = [];
+  let unfilled = {};
+
+  connection.query('select distinct responses.response_id, responses.patient_id, created_on, prescription_date, response_details.response_text from responses inner join patients on responses.patient_id = patients.patient_id inner join response_details on responses.response_id = response_details.response_id order by responses.patient_id;', (error, results, fields) => {
+    let data = [];
+    let current = null;
+    results.forEach((result) => {
+      if(current == null){
+        current = cleanResult(current, result);
+      }
+      else if(current.patient_id == result.patient_id){
+        current.responses.push({
+          response_id: result.response_id,
+          created_on: result.created_on,
+          text: result.response_text,
+        });
+      }
+      else{
+        data.push(current);
+        current = cleanResult(current, result);
+      }
+    });
+    data.push(current);
+    console.log(data);
+    unfilled.data = filterData(data);
+    console.log(JSON.stringify(unfilled));
+		mailOptions.html = JSON.stringify(unfilled);
+  	res.setHeader('Content-Type', 'application/json');
+  	res.send(JSON.stringify(unfilled));
+  	transporter.sendMail(mailOptions, (err, info) => {
+    	if(err)
+      	console.log(err);
+    	else
+      	console.log(info);
+  	});
+  });
 
   // unfilled = QUERY_RESULT
 
   //Construct JSON data
 
-  mailOptions.html = JSON.stringify({data: unfilled});
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({data: unfilled}));
-  transporter.sendMail(mailOptions, (err, info) => {
-    if(err)
-      console.log(err);
-    else
-      console.log(info);
-  });
 });
 
 //Listen
