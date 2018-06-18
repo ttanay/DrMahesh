f86777145bf2dab9ede437a1daf931516d39d9b5
ttanay <ttanay100@gmail.com>
2018-06-18 23:25:00 +0530

    single response

--- app.js
+++ app.js
@@ -6,28 +6,27 @@ const credentials = require('./credentials.js');
 app = express();
 
 //helpers
-var cleanResult = function(current, result){
-  current = {
+var cleanResult = function(result){
+  var cleanedResult  = {
     patient_id: result.patient_id,
     prescription_date: result.prescription_date,
-    responses: [],
+    response: null
   };
 	if(result.response_id != null){
-			current.responses.push({
+			cleanedResult.response = {
         response_id: result.response_id,
         created_on: result.created_on,
         text: result.response_text,
-      });
+      };
 	}
-  return current;
+  return cleanedResult;
 };
 
 var filterData = function(dataset){
   let filteredData = [];
   dataset.forEach((data) => {
-    let recentResponses = data.responses.slice(Math.max(data.responses.length -2, 1))
-    if(recentResponses >= 1){
-      let daysSinceSurvey = Math.abs(Date.parse(recentResponses[0]) - (new Date()));
+    if(data.response != null){
+      let daysSinceSurvey = Math.abs(Date.parse(data.response.created_on) - (new Date()));
       if(daysSinceSurvey > 7*5)
         filteredData.push(data);
     }
@@ -90,36 +89,24 @@ app.get('/sendmail/', (req, res) => {
   let unfilled = {};
 
   connection.query('select distinct responses.response_id, patients.patient_id, created_on, prescription_date, response_text from patients left join responses on responses.patient_id = patients.patient_id left join response_details on responses.response_id = response_details.response_id order by patients.patient_id;', (error, results, fields) => {
+    console.log(results)
+    console.log('------------------------------')
     let data = [];
-    let current = null;
     results.forEach((result) => {
-      if(current == null){
-        current = cleanResult(current, result);
-      }
-      else if(current.patient_id == result.patient_id){
-        current.responses.push({
-          response_id: result.response_id,
-          created_on: result.created_on,
-          text: result.response_text,
-        });
-      }
-      else{
-        data.push(current);
-        current = cleanResult(current, result);
-      }
+      result = cleanResult(result)
+      data.push(result)
     });
-    data.push(current);
     console.log(data);
     unfilled.data = filterData(data);
     console.log(JSON.stringify(unfilled));
-		mailOptions.html = JSON.stringify(unfilled);
+	mailOptions.html = JSON.stringify(unfilled);
   	res.setHeader('Content-Type', 'application/json');
   	res.send(JSON.stringify(unfilled));
   	transporter.sendMail(mailOptions, (err, info) => {
     	if(err)
-      	console.log(err);
+      	  console.log(err);
     	else
-      	console.log(info);
+      	  console.log(info);
   	});
   });
 
